2da2f87c53ac4e75622b2ef9e534ed6dbc403473
KaLuLas <kalulas41@gmail.com>
2018-12-04 20:08:12 +0800

    分册区分/初次标注完成
    现在记录了初次出现人名时在文本的对应行数

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ half_done 存放预处理文本素材
 
 pre_treatment.py 文本材料预处理，删去空行，标题，页注，并去除空行
 
-human_name_extraction.py 人名提取，之后添加人名唯一识别、初次出现标注、多文本区分功能
+human_name_extraction.py 人名提取，按分册进行，标记初次登场位置，人名，性别：之后添加人名唯一识别
--- human_name_extraction.py
+++ human_name_extraction.py
@@ -1,19 +1,16 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 17 13:32:20 2017
 
-@author: anjalidharmik
-"""
-
-import codecs
+import os
 import nltk
+import codecs
 import pandas as pd
 from nltk.corpus import names
-from operator import itemgetter
+
+path = ".\\half_done\\"
 
 
 def extract_name_and_gender(file_name):
-    name_l = []
+    name_dict = {}
     list_of_names =[]
     list_of_male_names = names.words('male.txt')
     list_of_female_names = names.words('female.txt')
@@ -22,33 +19,43 @@ def extract_name_and_gender(file_name):
 
     doc_str = codecs.open(file_name, "r", 'utf-8', 'ignore').read()
     cap_val_list = doc_str.split('\r\n')
-    for line in cap_val_list:
-        line = line.replace('Page', '').split(' ')
+    for line in enumerate(cap_val_list):
+        index = line[0]
+        line = line[1].replace('Page', '').split(' ')
         bialist = nltk.bigrams(line)
-
         save = ''
         for (first, last) in bialist:
-            if first in list_of_names and first != save :
+            if first in list_of_names and first != save:
                 gender = ''
                 if first in list_of_male_names:
                     gender = 'M'
                 elif first in list_of_female_names:
                     gender = 'F'
                 if last in list_of_names:
-                    name_l.append((first + ' ' + last, gender))
+                    if first + ' ' + last not in name_dict.keys():
+                        name_dict[first + ' ' + last] = (index, gender)
                     save = last
                 else:
-                    name_l.append((first, gender))
+                    if first not in name_dict.keys():
+                        name_dict[first] = (index, gender)
 
-    if len(name_l) >= 1:
+    if len(name_dict) >= 1:
         filename = (file_name.split('\\')[-1]).split('.pgr')[0]
-        name_l = sorted(set(name_l), key=itemgetter(0))
-        for name_t in name_l:
-            name = name_t[0]
-            gender = name_t[1]
-            df_out = pd.DataFrame({"Filename": [filename], "Name": [name], "Gender": [gender]})
+        name_dict = dict(sorted(name_dict.items(), key=lambda x: x[1][0]))
+        for name, value in name_dict.items():
+            index = value[0]
+            gender = value[1]
+            df_out = pd.DataFrame({"Filename": [filename], "Line": [index], "Name": [name], "Gender": [gender]})
             print(df_out)
 
 
-file_name = '.\half_done\\raw_input.txt'
-extract_name_and_gender(file_name)
+files = os.listdir(path)
+for file in files:
+    if file.startswith('book'):
+        print(file)
+        extract_name_and_gender(path + file)
+    else:
+        pass
+print("finished")
+
+
